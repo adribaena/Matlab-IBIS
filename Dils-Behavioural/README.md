942f994c14648f083c005ddf05b9df889c7c8443
adribaena <toni.rbaena@gmail.com>
2016-12-10 02:07:33 +0100

    changed load .mat

--- Dils-Behavioural/README.md
+++ Dils-Behavioural/README.md
@@ -5,3 +5,7 @@ For generating a .mat with the RT, the Group Name and the Acc numbe of trials.
 First to all, you need a folder structure like the example given.
 
 Then you just need to change the filters and the input / output folder (lines 8 to 13)
+
+
+Load "create_Behav_structure" to build a .mat file.
+Load "load_mat_structure" to plot the data of the group you want
--- Dils-Behavioural/load_mat_structure.m
+++ Dils-Behavioural/load_mat_structure.m
@@ -1,8 +1,32 @@
 function load_mat_structure
 
-% cargar un dils_structure.mat
+clear all
+clc
 
-% pendiente
+% definde group
+group = 'Controles';
+
+% cargar un dils_structure.mat
+load('dils_structure.mat')
+
+% pendiente de revisar y terminar
+
+%% no terminado de comentar
+
+indexGroup = find(strcmp(dils_data.group, group));
+
+reactionTimeGroup = dils_data.meansRT{indexGroup};
+accGroup = dils_data.ACC{indexGroup};
+
+plot(1:21,reactionTimeGroup,'*')
+hold on
+plot(1:21,reactionTimeGroup)
+
+%lower limit
+limsy=get(gca,'YLim');
+set(gca,'Ylim',[0 limsy(2)]);
+
+
 
 
 end
